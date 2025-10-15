c08ebf0aa06697b302a70a36b268761eaf1cec7a
Kevin Woods <kevinwoods@Kevins-MacBook-Pro.local>
2025-10-14 21:36:49 -0400

    Added Flask API deployment

---
+++ app/mlb_predict.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load the pre-trained model
+model = joblib.load('../models/mlb_salary_model.joblib')
+
+@app.route("/")
+def home():
+    return jsonify({"message": "MLB OBP Prediction API is running!"})
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+
+    # Create DataFram from input
+    df = pd.DataFrame([data])
+
+    # Predict OBP
+    predicted_obp = model.predict(df)[0]
+
+    return jsonify({
+        "predicted_OBP": round(float(predicted_obp), 3)
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
